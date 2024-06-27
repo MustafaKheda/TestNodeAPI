d803d37b415c21ccf3c9d505b4d615252c9054fc
Mustafa Kheda <mustafa.kheda@kadellabs.com>
2024-06-27 14:27:09 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ import connectDB from "./db/index.js";
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
 });
-connectDB()
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => {
-    console.log(err, "MongoDB connection failed");
-  });
+// connectDB()
+//   .then(() => {
+//     console.log("MongoDB connected");
+//   })
+//   .catch((err) => {
+//     console.log(err, "MongoDB connection failed");
+//   });
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
