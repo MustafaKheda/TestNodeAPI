de93a37888d71453dfc56ceda5ac3274aea6677e
Mustafa Kheda <mustafa.kheda@kadellabs.com>
2024-06-27 15:56:02 +0530

    Test v2.2.2

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ app.listen(8000, () => {
   console.log("Server is running on port 8000");
 });
 
-connectDB()
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => {
-    console.log(err, "MongoDB connection failed");
-  });
+// connectDB()
+//   .then(() => {
+//     console.log("MongoDB connected");
+//   })
+//   .catch((err) => {
+//     console.log(err, "MongoDB connection failed");
+//   });
 
 // const express = require('express');
 // const app = express();
