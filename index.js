11fbc063e220bdc64f67bae440dbec4051d34085
Mustafa Kheda <mustafa.kheda@kadellabs.com>
2024-06-27 16:28:15 +0530

    Test V2.3

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import userRouter from "./routes/user.route.js";
 const app = express();
-import cors from "cors";
+// import cors from "cors";
 
-app.use(express.json());
-app.use(cors());
-app.use("/api/v1/users", userRouter);
+// app.use(express.json());
+// app.use(cors());
+// app.use("/api/v1/users", userRouter);
+// import connectDB from "./db/index.js";
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
