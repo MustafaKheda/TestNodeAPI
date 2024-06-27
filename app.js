8f7158facaf747bca9b251403726ddab53805fe2
Mustafa Kheda <mustafa.kheda@kadellabs.com>
2024-06-27 15:06:37 +0530

    Test v2

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import userRouter from "./routes/user.route.js";
 const app = express();
 import cors from "cors";
+
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1/users", userRouter);
--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-// import { app } from "./app.js";
-// import connectDB from "./db/index.js";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
 
-// app.listen(8000, () => {
-//   console.log("Server is running on port 8000");
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
+
+connectDB()
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log(err, "MongoDB connection failed");
+  });
+
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 4000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
 // });
-// connectDB()
-//   .then(() => {
-//     console.log("MongoDB connected");
-//   })
-//   .catch((err) => {
-//     console.log(err, "MongoDB connection failed");
-//   });
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 4000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
