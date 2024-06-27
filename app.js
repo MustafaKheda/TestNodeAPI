e0499ff7cc01da8e49c57522ada090ddfa2327f5
Mustafa Kheda <mustafa.kheda@kadellabs.com>
2024-06-27 15:11:46 +0530

    Test v2.1

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ import cors from "cors";
 
 app.use(express.json());
 app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 app.use("/api/v1/users", userRouter);
 
 export { app };
