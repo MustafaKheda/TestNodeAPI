a1c5f177031b5a7cbf2ec702d446d3019fec0913
Mustafa Kheda <mustafa.kheda@kadellabs.com>
2024-06-27 15:25:51 +0530

    Test v2.2

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ connectDB()
 // const app = express();
 // const port = process.env.PORT || 4000;
 
-// app.get('/', (req, res) => {
-//   res.send('Hello, World!');
-// });
+
 
 // app.listen(port, () => {
 //   console.log(`Server is running on port ${port}`);
